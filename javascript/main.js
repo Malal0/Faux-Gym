cf9228d4fdef4e5327e5710cb8fc55934cdd9270
UncomfortableChai <mj1055119@gmail.com>
2022-04-14 20:23:32 -0700

    working on the navbar

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,15 @@
+//  Variables
 const removeBtn = document.querySelector("#top-remove-btn");
 const topTerms = document.querySelector("#topterms");
+const menuBtn = document.querySelector("#menu-button");
 
+//  Click Functions
 removeBtn.addEventListener("click", (e) => {
     setTimeout(() => {
         topTerms.remove();
     }, 1);
 });
+
+menuBtn.addEventListener("click", (e) => {
+    console.log(e);
+})
