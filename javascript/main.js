179073b4627f10b9ed783d8d0cae007aca2e06ab
UncomfortableChai <mj1055119@gmail.com>
2022-06-23 12:19:35 -0700

    nuked webpage

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,1 @@
-//  Variables
-const removeBtn = document.querySelector("#top-remove-btn");
-const topTerms = document.querySelector("#topterms");
-const menuBtn = document.querySelector("#menu-button");
-const ul = document.querySelector("#ul");
-
-//  Click Functions
-removeBtn.addEventListener("click", (e) => {
-    setTimeout(() => {
-        topTerms.remove();
-    }, 1);
-});
-
-menuBtn.addEventListener("click", (e) => {
-    ul.classList.toggle("nodisplay");
-    ul.classList.toggle("ulFlex");
-    menuBtn.classList.toggle("open");
-});
+console.log("loaded");
