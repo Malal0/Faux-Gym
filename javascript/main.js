83ce068d5f5c73d7952bd0e09eceadc6a680655b
UncomfortableChai <mj1055119@gmail.com>
2022-09-16 05:55:46 -0700

    testing darkmode

--- javascript/main.js
+++ javascript/main.js
@@ -4,19 +4,13 @@ const toggleColorPref = document.getElementById('hero-btn');
 const pref = window.matchMedia('(prefers-color-scheme: dark)');
 console.log(pref); // TRUE if user prefers dark mode
 
-if (pref.media == '(prefers-color-scheme: dark)') {
-    body.classList.add('darkmode');
-} else {
-    body.classList.remove('darkmode');
-}
+// if (pref.media == '(prefers-color-scheme: dark)') {
+//     body.classList.add('darkmode');
+// } else {
+//     body.classList.remove('darkmode');
+// }
 
 toggleColorPref.addEventListener('click', () => {
-    /*
-    if pref = light
-    set pref to dark
-    else
-    set pref to light
-    */
     if (body.classList.contains('darkmode')) {
         body.classList.remove('darkmode');
     } else {
