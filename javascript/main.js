2bb35682ba0d3fd984d2b11ce43e01036940da87
UncomfortableChai <mj1055119@gmail.com>
2022-09-16 05:51:07 -0700

    added darkmode

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,27 @@
-console.log("loaded");
+const body = document.querySelector('body');
+const toggleColorPref = document.getElementById('hero-btn');
+
+const pref = window.matchMedia('(prefers-color-scheme: dark)');
+console.log(pref); // TRUE if user prefers dark mode
+
+if (pref.media == '(prefers-color-scheme: dark)') {
+    body.classList.add('darkmode');
+} else {
+    body.classList.remove('darkmode');
+}
+
+toggleColorPref.addEventListener('click', () => {
+    /*
+    if pref = light
+    set pref to dark
+    else
+    set pref to light
+    */
+    if (body.classList.contains('darkmode')) {
+        body.classList.remove('darkmode');
+    } else {
+        body.classList.add('darkmode');
+    }
+
+    console.log(pref.media)
+})
