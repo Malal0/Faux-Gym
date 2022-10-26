855e2546eaa75f912654f123f5fa8af8e331dc0b
UncomfortableChai <mj1055119@gmail.com>
2022-10-26 16:32:04 -0700

    corrected the javascript error

--- javascript/main.js
+++ javascript/main.js
@@ -17,3 +17,15 @@ toggleColorPref.addEventListener('click', () => {
         body.classList.add('darkmode');
     }
 })
+
+const menuBtn = document.getElementById('menu-btn');
+const navEl = document.querySelector('nav');
+const ctaContainer = document.getElementById('cta-container');
+const heroBtn = document.getElementById('hero-btn');
+
+menuBtn.addEventListener('click', () => {
+    // navEl.classList.toggle('mobile-menu-open');
+    // ctaContainer.classList.toggle('no-display');
+    // heroBtn.classList.toggle('no-display');
+    // body.classList.toggle('full-screen');
+})
