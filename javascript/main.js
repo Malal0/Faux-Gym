24aadc4967d30581277f1c0a3a558332989f6fcf
UncomfortableChai <mj1055119@gmail.com>
2022-11-01 22:56:24 -0700

    adjusted the search bar button functions

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,14 @@
 const body = document.querySelector('body');
-const toggleColorPref = document.getElementById('hero-btn');
-
 const pref = window.matchMedia('(prefers-color-scheme: dark)');
 console.log(pref); // TRUE if user prefers dark mode
+const toggleColorPref = document.getElementById('hero-btn');
+const searchBtn = document.getElementById('search-btn');
+const deleteBtn = document.getElementById('delete-btn');
+const searchInput = document.getElementById('searchInput');
+const menuBtn = document.getElementById('menu-btn');
+const navEl = document.querySelector('nav');
+const ctaContainer = document.getElementById('cta-container');
+const heroBtn = document.getElementById('hero-btn');
 
 if (pref) {
     body.classList.add('darkmode');
@@ -18,14 +24,18 @@ toggleColorPref.addEventListener('click', () => {
     }
 })
 
-const menuBtn = document.getElementById('menu-btn');
-const navEl = document.querySelector('nav');
-const ctaContainer = document.getElementById('cta-container');
-const heroBtn = document.getElementById('hero-btn');
+// menuBtn.addEventListener('click', () => {
+//     // navEl.classList.toggle('mobile-menu-open');
+//     // ctaContainer.classList.toggle('no-display');
+//     // heroBtn.classList.toggle('no-display');
+//     // body.classList.toggle('full-screen');
+// })
 
-menuBtn.addEventListener('click', () => {
-    // navEl.classList.toggle('mobile-menu-open');
-    // ctaContainer.classList.toggle('no-display');
-    // heroBtn.classList.toggle('no-display');
-    // body.classList.toggle('full-screen');
-})
+searchBtn.addEventListener('click', () => {
+    searchInput.focus();
+});
+
+deleteBtn.addEventListener('click', () => {
+    searchInput.value = '';
+    searchInput.focus();
+});
